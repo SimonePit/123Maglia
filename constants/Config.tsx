0d3d37ff74aefc4e7d622981c30d8c1e6695ff3d
Luca Sanna <luca.sanna@ericsson.com>
2022-12-05 14:32:04 +0100

    inserito local address ma commentato in config

--- constants/Config.tsx
+++ constants/Config.tsx
@@ -1,6 +1,11 @@
 export default {
     LoginBaseUrl:"https://easit.easitsrl.it:8643/AuthenticationAppWeb",
     URL:"https://easit.easitsrl.it:8643/SartoriaWeb/SartoriaApp",
+
+//    Local Address per test    
+//    LoginBaseUrl:"http://localhost:8080/AuthenticationAppWeb",
+//    URL:"http://localhost:8080/SartoriaApp",
+
     IS_LOGGED_IN:false,
     Username:'',
   };
